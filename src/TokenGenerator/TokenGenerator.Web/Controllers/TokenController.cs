9739e658ca19e30f6e2bef15921fccd5e553aac1
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-10-22 11:44:14 +0200

    pod demo

---
+++ src/TokenGenerator/TokenGenerator.Web/Controllers/TokenController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TokenGenerator.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet]
+        public dynamic Get()
+        {
+            return new
+            {
+                Guid = Guid.NewGuid().ToString(),
+                Expires = DateTime.Now.AddDays(1),
+                Issuer = Environment.MachineName
+            };
+        }
+    }
+}
